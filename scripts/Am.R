8876bbd1c8998a0df5ffb991e70ae2b1ea44d92e
MatthewSainsbury-Dale <58652976+MatthewSainsbury-Dale@users.noreply.github.com>
2021-09-24 07:43:04 +1000

    Tidied some script names

---
+++ scripts/Am.R
@@ -0,0 +1,10 @@
+# Just a simple controlling script to keep all.R as clean as possible
+
+source("scripts/Am_GZ.R")
+source("scripts/Am_data.R")
+source("scripts/Am_blocks.R")
+source("scripts/Am_BAUs.R")
+source("scripts/Am_data_blocks_plot.R")
+source("scripts/Am_georob.R")
+source("scripts/Am_FRK.R")
+source("scripts/Am_comparison_plot.R")
