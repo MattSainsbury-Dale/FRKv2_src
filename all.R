1e367b7bcba345d8a9b251a8e0aec2b629d69498
MatthewSainsbury-Dale <58652976+MatthewSainsbury-Dale@users.noreply.github.com>
2021-09-29 10:07:30 +1000

    Added run-time information for all sections

--- all.R
+++ all.R
@@ -1,4 +1,4 @@
-# Facilitates user input regardless of how this script was invoked
+## Facilitates user input regardless of how this script was invoked
 user_decision <- function(prompt) {
   
   if (interactive()) {
@@ -22,7 +22,6 @@ user_decision <- function(prompt) {
 quick <- user_decision("Do you wish to use very-low-dimensional representations of the models to quickly establish that the code is working (note that the generated results and plots will not exactly match those in the manuscript if you reply 'y', and you will need at least 32GB of RAM and/or swap space if you reply 'n')? (y/n)\n")
 quick <- quick == "y" # Convert to Boolean
 
-
 ## Install dependencies:
 install_depends <- user_decision("Do you want to automatically install package dependencies? (y/n)\n")
 if (install_depends == "y") {
@@ -30,7 +29,6 @@ if (install_depends == "y") {
   install_exact_versions <- install_exact_versions == "y" # Convert to Boolean
   source("scripts/Dependencies_install.R")
 }
-
 
 ## Check that the data has been downloaded: 
 downloaded_correctly <- c(
@@ -48,40 +46,31 @@ if(!all(downloaded_correctly)) {
     ))
 }
 
+## Helper functions for running the scripts and providing informative output
+print_start_msg <- function(section_name, section_number) {
+  cat(paste("\n\n######## STARTING", section_name, "OF SECTION", section_number, "#############\n\n"))
+}
+  
+print_run_time <- function(section_name, section_number, total_time) {
+  cat(paste("\nFINISHED", section_name, "OF SECTION", section_number, "IN", round(total_time["elapsed"] / 60, 4), "MINUTES.\n"))
+}
+  
+source_script <- function(script, section_name, section_number) {
+  print_start_msg(section_name, section_number)
+  total_time <- system.time(source(paste0("scripts/", script)))
+  print_run_time(section_name, section_number, total_time)
+  rm(list = setdiff(ls(), c("quick", "print_start_msg", "print_run_time")))
+  gc()
+}
 
 
+## Run the scripts: 
+source_script("Poisson_sim.R", "POISSON EXAMPLE", 3.1)
+source_script("Negbinom_sim.R", "NEGATIVE-BINOMIAL EXAMPLE", 3.1)
+source_script("Heaton.R", "HEATON COMPARISON", 3.3)
+source_script("MODIS.R", "MODIS COMPARISON", 4.1)
+source_script("Am.R", "AMERICIUM COMPARISON", 4.2)
+source_script("Sydney.R", "SYDNEY CHANGE-OF-SUPPORT EXAMPLE", 4.3)
+source_script("Chicago.R", "CHICAGO EXAMPLE", 4.4)
 
-cat("\n\n######## STARTING POISSON EXAMPLE OF SECTION 3.1 #############\n\n")
-source("scripts/Poisson_sim.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
 
-cat("\n\n######## STARTING NEGATIVE-BINOMIAL EXAMPLE OF SECTION 3.2 #############\n\n")
-source("scripts/Negbinom_sim.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
-
-cat("\n\n######## STARTING HEATON COMPARISON OF SECTION 3.3 #############\n\n")
-source("scripts/Heaton.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
-
-cat("\n\n######## STARTING MODIS COMPARISON OF SECTION 4.1 #############\n\n")
-source("scripts/MODIS.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
-
-cat("\n\n######## STARTING AMERICIUM COMPARISON OF SECTION 4.2 #############\n\n")
-source("scripts/Am.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
-
-cat("\n\n######## STARTING SYDNEY EXAMPLE OF SECTION 4.3 #############\n\n")
-source("scripts/Sydney.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
-
-cat("\n\n######## STARTING CHICAGO EXAMPLE OF SECTION 4.4 #############\n\n")
-source("scripts/Chicago.R")
-rm(list = setdiff(ls(), "quick"))
-gc()
