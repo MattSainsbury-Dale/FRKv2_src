15ef35babab5e27ea07a255f84c1e619e668270d
MattSainsbury-Dale <msdale@uow.edu.au>
2021-10-31 12:07:40 +1100

    Merged Negbinom into one script

--- scripts/Negbinom_sim.R
+++ scripts/Negbinom_sim.R
@@ -1,7 +1,3 @@
-## Toy example for spatial COS.
-
-source("scripts/Utility_fns.R")
-
 suppressMessages({
 library("FRK")
 library("sp")
@@ -10,14 +6,9 @@ library("ggpubr")
 library("dplyr")
 library("raster")
 library("RandomFields")
-
-## Some helper functions to construct SpatialPolygons used as data supports, 
-## BAUs and arbitrary prediction polygons, and plotting helper functions.
-source("./scripts/Negbinom_SpatialPolygon_fns.R")
-source("./scripts/Plotting_helpers/Plotting_helpers.R")
-source("./scripts/Plotting_helpers/Negbinom_plotting_fns.R")
-source("./scripts/Utility_fns.R")
 })
+
+source("scripts/Utility_fns.R")
 
 ## Use very-low-dimensional representations of the models to establish that the code works? 
 quick <- check_quick()
@@ -26,8 +17,153 @@ nres  <- if (quick) 2 else 3
 # Define how we wish to simulate the latent Y process
 simulation_method <- "trig_field"  # model
 
-
 RNGversion("3.6.0"); set.seed(1)
+
+
+# ---- Helper functions ----
+
+## Some helper functions to construct SpatialPolygons used as data supports, 
+## BAUs and arbitrary prediction polygons, and plotting helper functions.
+
+## Lower left-hand vertex is (a, b), side length s
+makeSquare <- function(a, b, s) {
+  
+  ## Start with lower left-hand vertex then continue clockwise
+  vertex_corners <- rbind(c(a, b), 
+                          c(a, b + s), 
+                          c(a + s, b + s), 
+                          c(a + s, b))
+  
+  square <- Polygon(vertex_corners)
+  
+  return(square)
+}
+
+## s is the side length of the grid squares, 
+## n is the number of grid squares in each dimension,
+makeGrid <- function (n, range0 = 1) { 
+  
+  ## range0 is the desired extent of the grid.
+  ## Define s, the side length of the squares, so that s*n = range = range0:
+  s <- range0 / n
+  
+  squares <- list()
+  i <- 1
+  for (b in seq(0, (n-1)*s, by = s)) {
+    for (a in seq(0, (n-1)*s, by = s)) {
+      square <- Polygons(list(makeSquare(a = a, b = b, s = s)), 
+                         paste0("square", i))
+      squares[[i]] <- square
+      i = i + 1
+    }
+  }
+  
+  square_grid <- SpatialPolygons(squares)
+  
+  return(square_grid)
+}
+
+
+## Shift coordinates by a: a is scalar or length-two vector
+shift.coordinates <- function(coords, a) {
+  
+  if (length(a) == 1) {
+    coords <- coords + a
+  } else if (length(a) == 2) {
+    coords <- coords + rep(a, each = nrow(coords))
+  } else 
+    stop("a should be a scalar or a vector of length 2.")
+}
+
+
+## Credit to this function goes to:
+# https://stackoverflow.com/a/4860337
+convex.poly <- function(nSides, area) 
+{
+  # Find the radius of the circumscribed circle, and the angle of each point if this was a regular polygon
+  radius <- sqrt((2*area)/(nSides*sin((2*pi)/nSides)))
+  angle <- (2*pi)/nSides
+  
+  # Randomize the radii/angles
+  radii <- rnorm(nSides, radius, radius/10)
+  angles <- rnorm(nSides, angle, angle/10) * 1:nSides
+  angles <- sort(angles)
+  
+  points <- list(x=NULL, y=NULL)
+  points$x <- cos(angles) * radii
+  points$y <- sin(angles) * radii
+  
+  # Find the area of the polygon
+  m <- matrix(unlist(points), ncol=2)
+  m <- rbind(m, m[1,])
+  current.area <- 0.5 * (sum(m[1:nSides,1]*m[2:(nSides+1),2]) - sum(m[1:nSides,2]*m[2:(nSides+1),1]))
+  
+  points$x <- points$x * sqrt(area/current.area)
+  points$y <- points$y * sqrt(area/current.area)
+  
+  ## Append the first set of points to the end, to "complete" the polygon
+  points$x <- append(points$x, points$x[1])
+  points$y <- append(points$y, points$y[1])
+  
+  ## Convert to matrix
+  points <- do.call(cbind, points)
+  
+  return (points)
+}
+
+## coords: list of matrices containing the coordinates of the polygons
+## This function is based on the simple example from vignette("sp")
+coords_to_SpatialPolygons <- function(coords) {
+  
+  ## Convert to Polygon
+  Poly_list <- lapply(coords, Polygon)
+  
+  ## Give each Polygon a name
+  names(Poly_list) <- paste0("s", 1:length(Poly_list))
+  
+  ## Convert to Polygons
+  Polygons_list <- lapply(seq_along(Poly_list), function(i) Polygons(list(Poly_list[[i]]), names(Poly_list)[i]))
+  
+  ## Convert to SpatialPolygons
+  newdata <- SpatialPolygons(Polygons_list, 1:length(Polygons_list))
+  
+  return(newdata)
+}
+
+
+
+change_font_size <- function(gg) {
+  gg + theme(axis.text = element_text(size = 16),
+             axis.title = element_text(size = 19), 
+             legend.text = element_text(size = 16), 
+             plot.title = element_text(hjust = 0.5, size = 19))
+}
+
+change_legend_width <- function(gg, width = 1.1) {
+  gg + theme(legend.key.width = unit(width, 'cm'))
+}
+
+## Remove y-axis labels/ticks for all but the left-most panel
+interior_plot <- function(gg) {
+  gg + rremove("ylab") + rremove("xlab") + rremove("y.text") + rremove("y.ticks")
+}
+exterior_plot <- function(gg) {
+  gg + rremove("ylab") + rremove("xlab")
+}
+
+create_figure_one_row_of_plots <- function(plot_list) {
+  plot_list[[1]] = plot_list[[1]] %>% exterior_plot
+  for (i in 2:length(plot_list)) {
+    plot_list[[i]] = plot_list[[i]] %>% interior_plot
+  }
+  
+  figure <- ggarrange(plotlist = plot_list, 
+                      nrow = 1, legend = "top", align = "hv") %>% 
+    annotate_figure(left   = text_grob(bquote(s[2]), size = 20, vjust = 1,  hjust = 2, rot = 90),
+                    bottom = text_grob(bquote(s[1]), size = 20, vjust = -1, hjust = -0.5))
+} 
+
+
 
 
 # ---- Construct BAUs, data supports, and arbitrary regions to predict over ----
